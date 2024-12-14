12f7ac920072a09b6565ed2cc3c89cbe2dcaa312
Juniorrod2 <85754811+Juniorrod2@users.noreply.github.com>
2024-12-14 05:19:40 -0300

    Adição da plotagem de scores para PLS-DA

--- R/ROPLS_plots.R
+++ R/ROPLS_plots.R
@@ -59,7 +59,7 @@ Plot_ropls_scores <- function(model,groups=vector(),comp=c(1,2),group_colors=NUL
 }
 
 
-Plot_scores <- function(model,groups=NULL,comp=c(1,2),point_size=4,group_colors=NULL,show_labels=F,plot_theme=ggpubr::theme_pubr()){
+Plot_scores <- function(model,groups=NULL,comp=c(1,2),point_size=4,ellipse=T,labels=T,color_labels=F,point=T,repel_labels=F,theme=ggpubr::theme_pubr()){
 
   if(model@typeC=="PCA"){
 
@@ -77,21 +77,77 @@ Plot_scores <- function(model,groups=NULL,comp=c(1,2),point_size=4,group_colors=
       Legend=F
     }
 
+    if(ellipse==F){
+      group_alpha <- 0
+    }
 
-
+    if(length(labels)==1&&labels==T){
+      plot_label="Samples"
+    }else{
+      if(length(labels)==1&&labels==F){
+        plot_label=NULL
+      }else{
+        PCA_data$Scores$Samples <- labels
+        plot_label="Samples"
+      }
+    }
+    if(point==F){
+      color_labels=T
+    }
 
     scores_plot <- ggscatter(PCA_data$Scores,x=paste("p",comp[1],sep = ""),y=paste("p",comp[2],sep = ""),color=col_group,
-              size = 4,label = "Samples",
-              font.label = c(14,"plain","black"))+
-      stat_ellipse(aes(color=eval(Group),fill=eval(Group)),geom = "polygon",alpha=group_alpha,show.legend = F)+
-      labs(x=paste("Comp",comp[1]," (",model@modelDF$R2X[comp[1]]*100,"%)"),
-           y=paste("Comp",comp[2]," (",model@modelDF$R2X[comp[2]]*100,"%)"))+
+              size = point_size,label = plot_label,point = point,
+              font.label = if(color_labels==F) c(14,"plain","black"),repel = repel_labels)+
+      stat_ellipse(aes(color=if(ellipse==T) eval(Group),fill=if(ellipse==T) eval(Group)),geom = "polygon",alpha=group_alpha,show.legend = F)+
+      labs(x=paste("PC",comp[1]," (",model@modelDF$R2X[comp[1]]*100,"%)"),
+           y=paste("PC",comp[2]," (",model@modelDF$R2X[comp[2]]*100,"%)"),
+           title = "PCA")+
       theme_bw()+theme(legend.text = element_text(size = 14),
                        axis.text = element_text(size=14),
                        axis.title = element_text(size = 14),
                        legend.position = "bottom")
 
+  }
+
+  if(model@typeC=="PLS-DA"){
+
+    PLS_data <- extract_ropls_data(model)
+    Group <- "Group"
+    col_group <- Group
+    group_alpha=0.3
+    Legend=T
+
+    if(ellipse==F){
+      group_alpha <- 0
     }
 
-  return(scores_plot+plot_theme)
+    if(length(labels)==1&&labels==T){
+      plot_label="Samples"
+    }else{
+      if(length(labels)==1&&labels==F){
+        plot_label=NULL
+      }else{
+        PLS_data$Scores$Samples <- labels
+        plot_label="Samples"
+      }
+    }
+    if(point==F){
+      color_labels=T
+    }
+
+    scores_plot <- ggscatter(PLS_data$Scores,x=paste("p",comp[1],sep = ""),y=paste("p",comp[2],sep = ""),color=col_group,
+                             size = point_size,label = plot_label,point = point,
+                             font.label = if(color_labels==F) c(14,"plain","black"),repel = repel_labels)+
+      stat_ellipse(aes(color=if(ellipse==T) eval(Group),fill=if(ellipse==T) eval(Group)),geom = "polygon",alpha=group_alpha,show.legend = F)+
+      labs(x=paste("Comp",comp[1]," (",model@modelDF$R2X[comp[1]]*100,"%)"),
+           y=paste("Comp",comp[2]," (",model@modelDF$R2X[comp[2]]*100,"%)"),
+           title = "PLS-DA")+
+      theme_bw()+theme(legend.text = element_text(size = 14),
+                       axis.text = element_text(size=14),
+                       axis.title = element_text(size = 14),
+                       legend.position = "bottom")
+
+  }
+
+  return(scores_plot+theme)
 }
